9c1dd324ee27bb028603dcb3cb2a86938daae5e8
Sebastien Rousseau <sebastian.rousseau@gmail.com>
2026-01-21 20:17:45 +0000

    fix: correctly format clarity analytics script in vuepress config

--- docs/.vuepress/config/head/index.ts
+++ docs/.vuepress/config/head/index.ts
@@ -89,12 +89,15 @@ export const headers: any[] = [
   ["meta", { name: "twitter:url", content: "https://dotfiles.io/" }],
   ["meta", { name: "twitter:image", content: "/logo.png" }],
   [
-    (function (c, l, a, r, i, t, y) {
-      c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
-      t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i;
-      y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
+    "script",
+    {},
+    `
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
     })(window, document, "clarity", "script", "ejvjin4amu");
-`,
+    `,
   ],
   [
     "script",
